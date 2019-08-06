c43c92e7f3bae76eb01d31fde2717a0980ed480c
M. Ridwan Zalbina <zalbinaridwan@gmail.com>
2019-08-06 10:11:24 +0700

    salah

--- app/src/main/java/com/example/scafolmobile/interfacemodule/ItemClickListener.java
+++
@@ -1,9 +0,0 @@
-package com.example.scafolmobile.interfacemodule;
-
-import android.view.View;
-
-public interface ItemClickListener {
-    void onItemClick(View view, int position);
-    void onItemDoubleCLick(View view, int position);
-    void onDoubleClick(View view, int position);
-}
--- app/src/main/java/com/example/scafolmobile/sharedexternalmodule/DateInfo.java
+++
@@ -1,4 +0,0 @@
-package com.example.scafolmobile.sharedexternalmodule;
-
-public class DateInfo {
-}
