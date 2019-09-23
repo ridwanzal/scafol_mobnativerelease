8b2c2a587a73c5fd6f9039f087451fc98cff8410
M. Ridwan Zalbina <zalbinaridwan@gmail.com>
2019-09-23 14:18:29 +0700

    Commit Activity dan Adapter kurva s rencana

---
+++ app/src/main/java/com/release/activity/ActivityKurvaSRencana.java
@@ -0,0 +1,44 @@
+package com.release.activity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import com.release.restapi.ApiClient;
+import com.release.restapi.ApiInterface;
+
+public class ActivityKurvaSRencana extends AppCompatActivity {
+    private static String TAG = "ActivityKurvaSRencana";
+    ApiInterface apiInterface = ApiClient.getClient().create(ApiInterface.class);
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+}
---
+++ app/src/main/java/com/release/adapter/KurvaSRencanaAdapter.java
@@ -0,0 +1,10 @@
+package com.release.adapter;
+
+import com.release.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class KurvaSRencanaAdapter {
+
+}
